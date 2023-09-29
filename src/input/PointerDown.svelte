e39525e8d54cde75b1794ddebe46f453508f08ce
Phytoplancton <jonas-ullmann@web.de>
2023-09-29 23:33:58 +0200

    added PointerDown input

---
+++ src/input/PointerDown.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let isPointerDown: boolean;
+
+    window.addEventListener("pointerDown", () => {
+        isPointerDown = true;
+    });
+    window.addEventListener("pointerUp", () => {
+        isPointerDown = false;
+    });
+</script>
