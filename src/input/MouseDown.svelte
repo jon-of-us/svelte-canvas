59e1bd18baae714cf2e2eb24c45f77248b8bb872
Phytoplancton <jonas-ullmann@web.de>
2023-09-30 00:53:46 +0200

    changed mouse behavior

---
+++ src/input/MouseDown.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let isMouseDown: boolean;
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        window.addEventListener("mousedown", () => {
+            isMouseDown = true;
+        });
+        window.addEventListener("mouseup", () => {
+            isMouseDown = false;
+        });
+        window.addEventListener("touchstart", () => {
+            isMouseDown = true;
+        });
+        window.addEventListener("touchend", () => {
+            isMouseDown = false;
+        });
+    });
+</script>
