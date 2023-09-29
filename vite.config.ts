1d54c722f3439166a3839e7ff05fc087b8d9c19b
Phytoplancton <jonas-ullmann@web.de>
2023-09-29 16:02:20 +0200

    changed repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/svelte-canvas-template/",
+    base: "/svelte-canvas/",
     plugins: [svelte()],
 });
